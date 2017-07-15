19cab64e447dad6f405774c3df401185611a32e1
timurista <timothy.urista@gmail.com>
2017-07-15 16:26:19 -0700

    updated packages and added routing to support apollo graphql

--- graphql-sample/src/main/resources/client/App.jsx
+++ graphql-sample/src/main/resources/client/App.jsx
@@ -1,12 +1,24 @@
 /* eslint-disable */
 import React, {Component} from 'react'
 import Main from './components/Main.jsx';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 
-class App extends Component {
+import ApolloClient from 'apollo-client'; // integrates with server
+import { ApolloProvider } from 'react-apollo'; // integrates with react
 
-  render() {
-    return <Main/>
-  }
+// no backend assumptions, to configure later
+const client = new ApolloClient({});
+
+const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={Main}>
+          <IndexRoute component={Main} />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  );
 }
 
-export default App
+export default App;
--- graphql-sample/src/main/resources/webpack.config.js
+++ graphql-sample/src/main/resources/webpack.config.js
@@ -1,24 +1,35 @@
 /* eslint-disable */
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './client/index.js',
   output: {
     path: path.resolve('static'),
-    filename: 'index.js'
+    filename: 'bundle.js'
   },
   module: {
-    loaders: [
-      
+    rules: [
       {
-        test: /.jsx?$/,
         loader: 'babel-loader',
+        test: /\.js(x?)$/,
         exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
+        query:
+        {
+          presets:['react']
         }
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'client/index.html'
+    })
+  ]
 }
 
