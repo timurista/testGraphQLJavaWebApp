45961300ad20fcd6f9032e785266341f38af5556
Tim Urista <tim.urista@healthtrio.com>
2017-07-15 21:22:19 -0700

    master update GraphQLController.java in ../java/com

--- graphql-sample/src/main/java/com/merapar/graphql/sample/controller/GraphQLController.java
+++ graphql-sample/src/main/java/com/merapar/graphql/sample/controller/GraphQLController.java
@@ -1,7 +1,11 @@
 package com.merapar.graphql.sample.controller;
 
-import com.merapar.graphql.sample.models.RecordsBody;
+import graphql.ExecutionResult;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +15,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GraphQLController {
 
-	@RequestMapping("/app/testUserQuery")
-	public @ResponseBody
-	RecordsBody testUserQuery() {
-		return new RecordsBody();
-	}
+	@RequestMapping(value = "/graphql", method = RequestMethod.POST)
+	@ResponseBody
+	public Object executeOperation(@RequestBody Map body) {
+		String query = (String) body.get("query");
+		Map<String, Object> variables = (Map<String, Object>) body.get("variables");
+		if (variables == null) {
+			variables = new LinkedHashMap<>();
+		}
+		// TODO: graphql.execute(body), need ot make graphQL object
+		ExecutionResult executionResult = null;
+		Map<String, Object> result = new LinkedHashMap<>();
 
+		// temporary null checker, to be replaced or removed
+		if (executionResult == null) {
+			result.put("Error", new Object());
+			return result;
+		}
+
+		if (executionResult.getErrors().size() > 0) {
+			result.put("errors", executionResult.getErrors());
+			System.out.printf("Errors: {}", executionResult.getErrors());
+		}
+		result.put("data", executionResult.getData());
+		return result;
+	}
 }
