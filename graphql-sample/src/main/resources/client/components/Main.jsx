507ca1a5a73e65c13039f7b9b391b2e8530e86e9
Tim Urista <tim.urista@healthtrio.com>
2017-07-15 17:45:08 -0700

    added to fix linter issues

--- graphql-sample/src/main/resources/client/components/Main.jsx
+++ graphql-sample/src/main/resources/client/components/Main.jsx
@@ -1,35 +1,35 @@
-/* eslint-disable */
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 import axios from 'axios';
+import gql from 'graphql-tag';
 
 const QUERY = `
 { users 
-	{ 
-		id 
-	} 
+  { 
+    id 
+  } 
 }
 `
 
 class Main extends Component {
-	handleResponse(response) {
-		alert(res);
-	}
+  handleResponse(response) {
+    alert(res);
+  }
 
   handleClick() {
-		axios.post('api/testUserQuery', { query: QUERY})
-		.then( (res) => this.handleResponse(res))
-		.catch( e => { throw new Error(e) })
+    axios.post('api/testUserQuery', { query: QUERY})
+      .then( (res) => this.handleResponse(res))
+      .catch( e => { throw new Error(e) })
   }
 
   render() {
     return (
       <div>
-				<p>App Container div</p>
-				<p>custom QUERY = {QUERY}</p>
-				<button onClick={this.handleClick}>
-					Submit Test query
-				</button>
-			</div>
+        <p>App Container div</p>
+        <p>custom QUERY = {QUERY}</p>
+        <button onClick={this.handleClick}>
+          Submit Test query
+        </button>
+      </div>
     );
   }
 }
